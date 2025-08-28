70a118de78e389a873fd9936503874f9317f186d
cahhi <chrissyrodgers20@gmail.com>
2025-08-28 17:46:12 -0500

    Completed all deliverables!

--- src/main/java/org/example/CustomDivideByZeroException.java
+++ src/main/java/org/example/CustomDivideByZeroException.java
@@ -14,29 +14,23 @@ public class CustomDivideByZeroException extends  Exception {
      */
     private double b;
 
-    /**
-     * Get the numerator of the failed division operation.
-     *
-     * @return The numerator of the failed division operation.
-     */
-    public double getA() {
-        return a;
-    }
-
-    /**
-     * Get the denominator of the failed division operation.
-     *
-     * @return The denominator of the failed division operation.
-     */
-    public double getB() {
-        return b;
-    }
 
     /**
      * Constructor for the CustomDivideByZeroException.
      */
-    public CustomDivideByZeroException() {
+    public CustomDivideByZeroException(double a, double b) {
         // TODO: Modify this constructor to accept and store the two values attempted to be divided
         super("Attempted to divide by zero");
+        this.a = a;
+        this.b = b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
     }
 }
+
